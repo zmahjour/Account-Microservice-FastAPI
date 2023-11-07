f3f4d3b0598cde294bddc5a3ca3d36769dca7878
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-07 16:37:56 +0330

    chore: define get_hashed_password and verify_password functions

--- utils/utils.py
+++ utils/utils.py
@@ -1,1 +1,8 @@
 from core.config import settings
+def get_hashed_password(password: str) -> str:
+    return settings.PASSWORD_CONTEXT.hash(password)
+
+
+def verify_password(password: str, hashed_pass: str) -> bool:
+    return settings.PASSWORD_CONTEXT.verify(password, hashed_pass)
+
