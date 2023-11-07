5f8ec740858ba7ea45e19bfdab1e0feeebe07dc7
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-07 16:41:52 +0330

    chore: import required modules and classes

--- api/api_v1/endpoints/users.py
+++ api/api_v1/endpoints/users.py
@@ -1,4 +1,9 @@
 from fastapi import APIRouter, status, HTTPException
+from fastapi.encoders import jsonable_encoder
+from schemas.users import UserRegister
+from db.database import users_collection
+from utils.utils import get_hashed_password, get_user
+
 
 router = APIRouter(prefix="/account", tags=["account"])
 
