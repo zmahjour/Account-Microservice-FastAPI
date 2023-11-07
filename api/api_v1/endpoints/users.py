71b1120976f616333eaa153f714424a5583aa7dc
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-07 16:42:42 +0330

    chore: define account_register api

--- api/api_v1/endpoints/users.py
+++ api/api_v1/endpoints/users.py
@@ -8,3 +8,28 @@ from utils.utils import get_hashed_password, get_user
 router = APIRouter(prefix="/account", tags=["account"])
 
 
+@router.post("/register", status_code=status.HTTP_201_CREATED)
+async def account_register(new_user: UserRegister):
+    user, detail = await get_user(username=new_user.username, email=new_user.email)
+
+    if user and detail == "username":
+        raise HTTPException(
+            status_code=status.HTTP_406_NOT_ACCEPTABLE,
+            detail=f"The username {new_user.username} is not available.",
+        )
+    elif user and detail == "email":
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"The email {new_user.email} is not available.",
+        )
+    elif not user and detail == "User not found.":
+        user = jsonable_encoder(new_user)
+        user["password"] = get_hashed_password(user["password"])
+        await users_collection.insert_one(user)
+
+        return {"detail": "You have registered successfully."}
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="There is a problem; try again.",
+        )
