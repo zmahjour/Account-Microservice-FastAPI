e4692b853ea6110a05cea863a6d975b5049dc2fc
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-07 16:41:21 +0330

    chore: define get_user function to get user from db with username or email

--- utils/utils.py
+++ utils/utils.py
@@ -9,3 +9,13 @@ def get_hashed_password(password: str) -> str:
 def verify_password(password: str, hashed_pass: str) -> bool:
     return settings.PASSWORD_CONTEXT.verify(password, hashed_pass)
 
+
+async def get_user(username: str, email: str):
+    user = await users_collection.find_one({"username": username})
+    if user:
+        return user, "username"
+    user = await users_collection.find_one({"email": email})
+    if user:
+        return user, "email"
+
+    return None, "User not found."
