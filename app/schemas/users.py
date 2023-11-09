652532b7937f826cfc7dc295517ead7bf619bb43
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-10 00:08:11 +0330

    refactor: remove _id from UserRegister and UserLogin

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -2,14 +2,12 @@ from pydantic import BaseModel, EmailStr
 
 
 class UserRegister(BaseModel):
-    _id: str
     username: str
     email: EmailStr
     password: str
 
 
 class UserLogin(BaseModel):
-    _id: str
     username: str | None
     email: EmailStr | None
     password: str
