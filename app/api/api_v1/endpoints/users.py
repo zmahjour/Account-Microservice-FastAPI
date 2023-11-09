93ad09aa24d711e40f731582882bb0cfe8f9dde8
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-10 00:08:28 +0330

    chore: import OtpEmail

--- app/api/api_v1/endpoints/users.py
+++ app/api/api_v1/endpoints/users.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, status, HTTPException
 from fastapi.encoders import jsonable_encoder
-from schemas.users import UserRegister, UserLogin
+from schemas.users import UserRegister, UserLogin, OtpEmail
 from db.database import users_collection
 from utils.utils import get_hashed_password, get_user, verify_password
 
